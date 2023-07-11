4f271111fb233b213bb90804e99123b617d31201
HannesZ <31802312+HannesZ@users.noreply.github.com>
2023-07-11 11:54:57 +0200

    Update lightGBM.py
     expose the parameter grid as an argument to the class constructor

--- lightGBM.py
+++ lightGBM.py
@@ -15,16 +15,18 @@ print("lightgbm version: " + sys.modules[module_name].__version__)
 
 
 class LGBMRegressorModel(RegressorModel):
+    
+    def __init__(self, param_grid=None):
+        if param_grid is None:
+            self.param_grid = default_LGBMRegressor_param_grid
+        else:
+            self.param_grid = param_grid
 
     def get_pipeline(self):
-        if self.param_grid is not None:
-            parameters = self.param_grid
-        else:
-            parameters = default_LGBMRegressor_param_grid
         pipeline = Pipeline([
-            ("regressor", LGBMRegressor(learning_rate=0.1, n_estimators=250, objective='regression', subsample=0.75, reg_lambda=0.06))
+            ("regressor", LGBMRegressor())
         ])
-        return pipeline, parameters
+        return pipeline, self.param_grid
 
 default_LGBMRegressor_param_grid = {
     'regressor__learning_rate': [0.02, 0.1],
@@ -32,20 +34,23 @@ default_LGBMRegressor_param_grid = {
     'regressor__objective': ['regression'],
     'regressor__boosting_type': ['gbdt', 'goss'],
     'regressor__subsample': [0.75],
-    'regressor__reg_lambda':[0.05]
+    'regressor__reg_lambda':[0.05, 0.08],
+    'classifier__random_state': [42]
 }
 
 
 
 class LGBMClassifier(ClassifierModel):
 
-    def get_pipeline(self):
-        if self.param_grid is not None:
-            parameters = self.param_grid
+    def __init__(self, param_grid=None):
+        if param_grid is None:
+            self.param_grid = default_LGBMClassifier_param_grid
         else:
-            parameters = default_LGBMClassifier_param_grid
+            self.param_grid = param_grid
+
+    def get_pipeline(self):
         pipeline = Pipeline([
-            ("classifier", LGBMClassifier(learning_rate=0.1, n_estimators=250, objective='multiclass', subsample=0.75, reg_lambda=0.06))
+            ("classifier", LGBMClassifier())
         ])
         return pipeline, parameters
 
@@ -54,6 +59,7 @@ default_LGBMClassifier_param_grid = {
     'classifier__n_estimators': [250, 350],
     'classifier__objective': ['multiclass'],
     'classifier__subsample': [0.75, 0.8],
-    'classifier__reg_lambda':[0.05, 0.08]
+    'classifier__reg_lambda':[0.05, 0.08],
+    'classifier__random_state': [42]
 }
 
